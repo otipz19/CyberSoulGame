148af1b09485329f7cd09997faec7b8d16765285
otipz19 <tasitwork@gmail.com>
2024-05-22 14:36:53 +0300

    WiP: adding Box2D

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -15,114 +15,162 @@ import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import org.w3c.dom.css.Rect;
 
 public class MyGdxGame implements ApplicationListener {
-	private static final String TILEMAP_FILE_NAME = "first-level.tmx";
+    private static final String TILEMAP_FILE_NAME = "first-level.tmx";
 
-	private TiledMap map;
-	private OrthogonalTiledMapRenderer mapRenderer;
-	private OrthographicCamera camera;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
 
-	private AssetManager assetManager;
-	private SpriteBatch batch;
-	private SimpleActor simpleActor;
+    private AssetManager assetManager;
+    private SpriteBatch batch;
+    private SimpleActor simpleActor;
 
-	private Texture debugTexture;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		loadAssets();
-		createMap();
-		createCamera();
-		createMapRenderer();
+    private Texture debugTexture;
+
+    public Array<Shape2D> colliders;
+
+    public World world;
+    private float accumulator;
+    private static final float TIME_STEP = 1 / 60f;
+    private static final int VELOCITY_ITERATIONS = 6;
+    private static final int POSITION_ITERATIONS = 2;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        loadAssets();
+        createMap();
+        createCamera();
+        createMapRenderer();
 //		Vector3 pos = camera.unproject(new Vector3(400, 600, 0));
-		simpleActor = new SimpleActor(this, 10, 10);
-		debugTexture = assetManager.get("background-1.png");
-	}
+        simpleActor = new SimpleActor(this, 10, 10);
+        debugTexture = assetManager.get("background-1.png");
+		colliders = new Array<>();
+        for (MapObject mapObject : map.getLayers().get("colliders").getObjects()) {
+            if (mapObject instanceof RectangleMapObject) {
+                Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
+                float x = rectangle.x / 32;
+                float y = rectangle.y / 32;
+                float width = rectangle.width / 32;
+                float height = rectangle.height / 32;
+                Rectangle collider = new Rectangle(x, y, width, height);
+                colliders.add(collider);
+            } else if (mapObject instanceof PolygonMapObject) {
+                Polygon polygon = ((PolygonMapObject) mapObject).getPolygon();
+                float[] screenVertices = polygon.getTransformedVertices();
+                float[] worldVertices = new float[screenVertices.length];
+                for (int i = 0; i < screenVertices.length; i += 2) {
+					worldVertices[i] = screenVertices[i] / 32;
+					worldVertices[i + 1] = screenVertices[i + 1] / 32;
+                }
+				Polygon collider = new Polygon(worldVertices);
+				colliders.add(collider);
+            }
+        }
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+    }
 
-	private void loadAssets(){
-		assetManager = new AssetManager();
-		assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
-		assetManager.load(TILEMAP_FILE_NAME, TiledMap.class);
-		assetManager.load("hero.png", Texture.class);
-		assetManager.load("background-1.png", Texture.class);
-		assetManager.finishLoading();
-	}
+    private void loadAssets() {
+        assetManager = new AssetManager();
+        assetManager.setLoader(TiledMap.class, new TmxMapLoader(new InternalFileHandleResolver()));
+        assetManager.load(TILEMAP_FILE_NAME, TiledMap.class);
+        assetManager.load("hero.png", Texture.class);
+        assetManager.load("background-1.png", Texture.class);
+        assetManager.finishLoading();
+    }
 
-	private void createMap() {
-		map = assetManager.get(TILEMAP_FILE_NAME);
-	}
+    private void createMap() {
+        map = assetManager.get(TILEMAP_FILE_NAME);
+    }
 
-	private void createCamera() {
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 30, 20);
-	}
+    private void createCamera() {
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 15, 10);
+//		camera.zoom = 0.5f;
+        camera.update();
+    }
 
-	private void createMapRenderer() {
-		mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
-		mapRenderer.setView(camera);
-	}
+    private void createMapRenderer() {
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / 32f);
+        mapRenderer.setView(camera);
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(Color.WHITE);
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(debugTexture, 0, 0, 30, 20);
-		batch.end();
-		mapRenderer.render();
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		simpleActor.render();
-//		for(MapObject mapObject: map.getLayers().get("colliders").getObjects()){
-//			if(mapObject instanceof RectangleMapObject){
-//				Rectangle rectangle = ((RectangleMapObject)mapObject).getRectangle();
-//				batch.draw(debugTexture, rectangle.getX(), rectangle.getY(), rectangle.width, rectangle.height);
-//			} /*else if (mapObject instanceof PolygonMapObject){
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.WHITE);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.draw(debugTexture, 0, 0, 30, 20);
+        batch.end();
+        mapRenderer.render();
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        simpleActor.render();
+//        for (MapObject mapObject : map.getLayers().get("colliders").getObjects()) {
+//            if (mapObject instanceof RectangleMapObject) {
+//                Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
+//                Vector3 worldPos = new Vector3(rectangle.getX() / 32, rectangle.getY() / 32, 0);
+//                Vector3 worldSize = new Vector3(rectangle.width / 32, rectangle.height / 32, 0);
+//                batch.draw(debugTexture, worldPos.x, worldPos.y, worldSize.x, worldSize.y);
+//            } /*else if (mapObject instanceof PolygonMapObject){
 ////				Polygon polygon = ((PolygonMapObject)mapObject).getPolygon();
 ////				Rectangle bounds = polygon.getBoundingRectangle();
 ////				batch.draw(debugTexture, bounds.x, bounds.y, bounds.width, bounds.height);
 ////			}*/
-//		}
-		batch.end();
-	}
+//        }
+        batch.end();
+		camera.position.set(simpleActor.bounds.x, simpleActor.bounds.y, 0);
+        mapRenderer.setView(camera);
+		camera.update();
 
-	@Override
-	public void pause() {
+        accumulator += Gdx.graphics.getDeltaTime();
+        while(accumulator >= TIME_STEP){
+            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+            accumulator -= TIME_STEP;
+        }
+    }
 
-	}
+    @Override
+    public void pause() {
 
-	@Override
-	public void resume() {
+    }
 
-	}
+    @Override
+    public void resume() {
 
-	@Override
-	public void resize(int width, int height) {
+    }
 
-	}
+    @Override
+    public void resize(int width, int height) {
 
-	@Override
-	public void dispose () {
-		assetManager.dispose();
-	}
+    }
 
-	public SpriteBatch getBatch(){
-		return batch;
-	}
+    @Override
+    public void dispose() {
+        assetManager.dispose();
+    }
 
-	public AssetManager getAssetManager(){
-		return assetManager;
-	}
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 
-	public TiledMap getMap(){
-		return map;
-	}
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
 
-	public OrthographicCamera getCamera(){
-		return camera;
-	}
+    public TiledMap getMap() {
+        return map;
+    }
+
+    public OrthographicCamera getCamera() {
+        return camera;
+    }
 }
--- core/src/com/mygdx/game/SimpleActor.java
+++ core/src/com/mygdx/game/SimpleActor.java
@@ -11,18 +11,38 @@ import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Shape2D;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class SimpleActor {
     private MyGdxGame game;
     private Texture sprite;
-    private Rectangle bounds;
+    public Rectangle bounds;
     private float horizontalVelocity = 10f;
     private float gravity = 10f;
+    public Body body;
 
     public SimpleActor(MyGdxGame game, float x, float y){
         this.game = game;
         sprite = game.getAssetManager().get("hero.png");
         bounds = new Rectangle(x, y, 1, 1.5f);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        bodyDef.position.set(x, y);
+        body = game.world.createBody(bodyDef);
+
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(0.5f, 0.75f);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1;
+        fixtureDef.friction = 1;
+        fixtureDef.restitution = 0;
+
+        Fixture fixture = body.createFixture(fixtureDef);
+        
+        shape.dispose();
     }
 
     public void render(){
@@ -35,19 +55,25 @@ public class SimpleActor {
             bounds.setY(bounds.getY() + delta(gravity * 2));
         }
         boolean hasCollided = false;
-        for(MapObject mapObject: game.getMap().getLayers().get("colliders").getObjects()){
-            Shape2D collider;
-            if(mapObject instanceof RectangleMapObject){
-                collider = ((RectangleMapObject)mapObject).getRectangle();
-            } else {
-                collider = ((PolygonMapObject)mapObject).getPolygon();
-            }
-            Vector3 projectedPos = game.getCamera().project(new Vector3(bounds.x, bounds.y, 0));
-            if(collider.contains(projectedPos.x, projectedPos.y)){
+        for(Shape2D collider: game.colliders){
+            if(collider.contains(bounds.x, bounds.y)){
                 hasCollided = true;
                 break;
             }
         }
+//        for(MapObject mapObject: game.getMap().getLayers().get("colliders").getObjects()){
+//            Shape2D collider;
+//            if(mapObject instanceof RectangleMapObject){
+//                collider = ((RectangleMapObject)mapObject).getRectangle();
+//            } else {
+//                collider = ((PolygonMapObject)mapObject).getPolygon();
+//            }
+//            Vector3 projectedPos = game.getCamera().project(new Vector3(bounds.x, bounds.y, 0));
+//            if(collider.contains(projectedPos.x, projectedPos.y)){
+//                hasCollided = true;
+//                break;
+//            }
+//        }
         if(!hasCollided){
             bounds.setY(bounds.getY() - delta(gravity));
         }
