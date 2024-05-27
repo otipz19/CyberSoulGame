157e4ac9acb752665f63d495ece474158e15499d
otipz19 <tasitwork@gmail.com>
2024-05-27 11:22:04 +0300

    Extracted level logic to TestLevel and abstract Level

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,55 +1,24 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.*;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.*;
-import com.mygdx.game.camera.LevelCamera;
-import com.mygdx.game.physics.Collider;
-import com.mygdx.game.physics.ColliderCreator;
+import com.mygdx.game.entities.Hero;
+import com.mygdx.game.levels.TestLevel;
 import com.mygdx.game.utils.AssetsNames;
-import com.mygdx.game.camera.CoordinatesProjector;
-import com.mygdx.game.utils.LevelObjectsParser;
 
 import java.util.stream.Stream;
 
-public class MyGdxGame implements ApplicationListener {
+public class MyGdxGame extends Game {
     private static MyGdxGame instance;
 
-    private TiledMap map;
-    private OrthogonalTiledMapRenderer mapRenderer;
-    private LevelCamera camera;
-    private ScreenViewport viewport;
-
     public AssetManager assetManager;
-    private SpriteBatch batch;
-    private SimpleActor simpleActor;
-
-    private Texture debugTexture;
-
-    public World world;
-    private float accumulator;
-    private static final float TIME_STEP = 1 / 60f;
-    private static final int VELOCITY_ITERATIONS = 6;
-    private static final int POSITION_ITERATIONS = 2;
-    private Box2DDebugRenderer box2dRenderer;
-
-    private int levelWidth;
-    private int levelHeight;
-    private float unitScale;
+    public SpriteBatch batch;
 
     public static MyGdxGame getInstance() {
         return instance;
@@ -58,19 +27,10 @@ public class MyGdxGame implements ApplicationListener {
     @Override
     public void create() {
         instance = this;
-
+        loadAssets();
         Box2D.init();
         batch = new SpriteBatch();
-
-        loadAssets();
-        createMap();
-        createCamera();
-
-        simpleActor = new SimpleActor(this, 17, 5);
-        camera.setPositionSharply(simpleActor.getCameraPosition());
-
-        debugTexture = assetManager.get(AssetsNames.GREENZONE_BACKGROUND_FULL);
-        box2dRenderer = new Box2DDebugRenderer(true, true, true, true, true, true);
+        setScreen(new TestLevel(this));
     }
 
     private void loadAssets() {
@@ -93,115 +53,13 @@ public class MyGdxGame implements ApplicationListener {
         assetManager.finishLoading();
     }
 
-    private void createMap() {
-        world = new World(new Vector2(0, -10), true);
-
-        map = assetManager.get(AssetsNames.TEST_LEVEL_TILEMAP);
-
-        MapProperties mapProperties = map.getProperties();
-        levelWidth = (int) mapProperties.get("width");
-        levelHeight = (int) mapProperties.get("height");
-        int tileSize = (int) mapProperties.get("tileheight");
-        unitScale = 1f / tileSize;
-        float mapHeight = tileSize * levelHeight;
-
-        mapRenderer = new OrthogonalTiledMapRenderer(map, unitScale);
-
-        LevelObjectsParser parser = new LevelObjectsParser(AssetsNames.TEST_LEVEL_TILEMAP, "colliders");
-        CoordinatesProjector coordinatesProjector = new CoordinatesProjector(unitScale, mapHeight);
-        for (Shape2D shape : parser.getShapes()) {
-            Collider collider;
-            if (shape instanceof Rectangle)
-                collider = ColliderCreator.create((Rectangle) shape, coordinatesProjector);
-            else if (shape instanceof Polygon)
-                collider = ColliderCreator.create((Polygon) shape, coordinatesProjector);
-            else
-                throw new RuntimeException("Shape is not supported");
-
-            BodyDef bodyDef = new BodyDef();
-            bodyDef.type = BodyDef.BodyType.StaticBody;
-            bodyDef.position.set(collider.getX(), collider.getY());
-
-            FixtureDef fixtureDef = new FixtureDef();
-            fixtureDef.shape = collider.getShape();
-            fixtureDef.friction = 1;
-            fixtureDef.density = 1;
-            fixtureDef.restitution = 0;
-
-            Body body = world.createBody(bodyDef);
-            body.createFixture(fixtureDef);
-
-            collider.dispose();
-        }
-    }
-
-
-    private void createCamera() {
-        camera = new LevelCamera(levelWidth, levelHeight);
-        camera.adjustZoomForViewportSize(levelWidth/2f, levelHeight/2f);
-
-        viewport = new ScreenViewport(camera);
-        viewport.setUnitsPerPixel(unitScale);
-    }
-
     @Override
-    public void render() {
-        ScreenUtils.clear(Color.WHITE);
-
-        camera.setPositionSmoothly(simpleActor.getCameraPosition());
-        camera.update();
-        mapRenderer.setView(camera);
-
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        batch.draw(debugTexture, 0, 0, 30, 20);
-        batch.end();
-        mapRenderer.render();
-        batch.begin();
-        simpleActor.render();
-        batch.end();
-        box2dRenderer.render(world, camera.combined);
-
-        accumulator += Gdx.graphics.getDeltaTime();
-        while (accumulator >= TIME_STEP) {
-            world.step(TIME_STEP, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
-            accumulator -= TIME_STEP;
-        }
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        viewport.update(width, height);
+    public void render(){
+        super.render();
     }
 
     @Override
     public void dispose() {
         assetManager.dispose();
-    }
-
-    public SpriteBatch getBatch() {
-        return batch;
-    }
-
-    public AssetManager getAssetManager() {
-        return assetManager;
-    }
-
-    public TiledMap getMap() {
-        return map;
-    }
-
-    public OrthographicCamera getCamera() {
-        return camera;
     }
 }
